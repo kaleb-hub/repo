1a863f050a7dd84b1970e4bbb5ae1de3ddbc006b
EshetuGC <82573927+EshetuGC@users.noreply.github.com>
2021-05-03 13:42:30 -1200

    well come this is edited part of threadexam1 class

--- threadexam1.java
+++ threadexam1.java
@@ -6,6 +6,7 @@ public class threadexam1 {
 		public void run() {
 			System.out.println("My thread started");
 			System.out.println("thread is one part of process");
+			System.out.println("Jesus will never let me down");
 		}
 		public static void main(String args[]) {
 			mythread t1=new mythread();
