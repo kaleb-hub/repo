b9803227553ba9d0e1d3a3085bd92e8fe2cc1b49
kaleb <82520535+kaleb-hub@users.noreply.github.com>
2021-05-04 08:26:40 +0300

    this is new version of ethiopia

--- src/index.java
+++ src/index.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 
 public class index {
-
+public static void main(String args[])
+{
+	Scanner se=new Scanner(System.in);
+	System.out.println("enter your name");
+	String name=se.next();
+	System.out.println("your name is:"+name);
+}
 }
