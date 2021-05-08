eb51f7842881fb981590b85662814a90b4d7436a
kaleb <82520535+kaleb-hub@users.noreply.github.com>
2021-05-08 04:36:42 +0300

    add method added

--- src/home.java
+++ src/home.java
@@ -5,4 +5,8 @@ public static void main(String args[])
 	System.out.println("this is home page");
 	System.out.println("welcome to online repository");
 }
+public void add()
+{
+	System.out.println("add method");
+}
 }
